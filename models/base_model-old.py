88dbdffbf76d8d62b1e0a7f62d46f69ed7c428e4
sayed azp <sayedazp@gmail.com>
2023-08-13 09:11:12 +0300

    fixing

--- models/base_model-old.py
+++ models/base_model-old.py
@@ -8,28 +8,32 @@ from datetime import datetime
 class BaseModel:
     """Represents the BaseModel of the HBnB project."""
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, **kwargs):
         """Initialize a new BaseModel.
 
         Args:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
         """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
         if len(kwargs) != 0:
             for k, v in kwargs.items():
+                if (k not in self.__dict__.keys()):
+                    pass
                 if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
+                    self.__dict__[k] = datetime.fromisoformat(v)
+                elif(k == '__class__'):
+                    pass
                 else:
                     self.__dict__[k] = v
         else:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Update updated_at with the current datetime and \
+        save the instance."""
         self.updated_at = datetime.today()
         models.storage.save()
 
